7294a96fcaaf4d3c8bd31e7fdabe9d1e2bdf4964
Li Ju <sheldon.juli@gmail.com>
2018-10-09 11:59:26 -0400

    Added pods

--- JumpAndSlide/JumpAndSlide/ViewControllers/GameViewController.swift
+++ JumpAndSlide/JumpAndSlide/ViewControllers/GameViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import GoogleMobileAds
 
 protocol SceneManagerDelegate {
     func presentMenuScene()
@@ -75,7 +76,7 @@ extension GameViewController: SceneManagerDelegate {
 }
 
 //extension GameViewController: GADBannerViewDelegate {
-//    
+//
 //    func addGADBanner() {
 //        let BannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
 //        BannerView.isHidden = true
@@ -88,12 +89,12 @@ extension GameViewController: SceneManagerDelegate {
 //        BannerView.backgroundColor = UIColor.black
 //        view.addSubview(BannerView)
 //    }
-//    
+//
 //    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
 //        print("Banner is receiving ads.")
 //        bannerView.isHidden = false
 //    }
-//    
+//
 //    func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
 //        print(error)
 //        bannerView.isHidden = true
@@ -101,121 +102,121 @@ extension GameViewController: SceneManagerDelegate {
 //}
 //
 //extension GameViewController: GADRewardBasedVideoAdDelegate, PlayRewardAdDelegate {
-//    
+//
 //    func checkIfRewardAdAvailable() -> Bool {
 //        return rewardBasedAd.isReady
 //    }
-//    
+//
 //    func playRewardAd() {
 //        if checkIfRewardAdAvailable() {
 //            rewardBasedAd.present(fromRootViewController: self)
 //        }
 //    }
-//    
+//
 //    func createAndLoadRewardBasedAd() -> GADRewardBasedVideoAd {
 //        let rewardBasedAd = GADRewardBasedVideoAd.sharedInstance()
 //        rewardBasedAd.delegate = self
 //        let request = GADRequest()
-//        
+//
 //        // TDOD remove test code
 //        request.testDevices = [ "b90d517bbd126ad43c612357a349ee23" ]
 //        rewardBasedAd.load(request, withAdUnitID: GoogleAdmobValues.rewardAdUnitID)
 //        return rewardBasedAd
 //    }
-//    
+//
 //    func rewardBasedVideoAd(_ rewardBasedVideoAd: GADRewardBasedVideoAd,
 //                            didRewardUserWith reward: GADAdReward) {
 //        print("Reward received with currency: \(reward.type), amount \(reward.amount).")
-//        
+//
 //        gameScene?.rewardUserForWatchingAd()
 //    }
-//    
+//
 //    func rewardBasedVideoAdDidReceive(_ rewardBasedVideoAd:GADRewardBasedVideoAd) {
 //        print("Reward based video ad is received.")
 //    }
-//    
+//
 //    func rewardBasedVideoAdDidOpen(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 //        print("Opened reward based video ad.")
 //    }
-//    
+//
 //    func rewardBasedVideoAdDidStartPlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 //        print("Reward based video ad started playing.")
 //    }
-//    
+//
 //    func rewardBasedVideoAdDidCompletePlaying(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 //        print("Reward based video ad has completed.")
 //    }
-//    
+//
 //    func rewardBasedVideoAdDidClose(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 //        print("Reward based video ad is closed.")
 //        rewardBasedAd = createAndLoadRewardBasedAd()
 //    }
-//    
+//
 //    func rewardBasedVideoAdWillLeaveApplication(_ rewardBasedVideoAd: GADRewardBasedVideoAd) {
 //        print("Reward based video ad will leave application.")
 //    }
-//    
+//
 //    func rewardBasedVideoAd(_ rewardBasedVideoAd: GADRewardBasedVideoAd,
 //                            didFailToLoadWithError error: Error) {
 //        print("Reward based video ad failed to load.")
 //    }
-//    
+//
 //}
 //
 //extension GameViewController: GADInterstitialDelegate, PlayInterstitialAdDelegate {
-//    
+//
 //    func checkIfInterstitialAdAvailable() -> Bool {
 //        return interstitialAd.isReady
 //    }
-//    
+//
 //    func playInterstitialAd() {
 //        if checkIfInterstitialAdAvailable() {
 //            interstitialAd.present(fromRootViewController: self)
 //        }
 //    }
-//    
+//
 //    func createAndLoadInterstitial() -> GADInterstitial {
 //        let interstitialAd = GADInterstitial(adUnitID: GoogleAdmobValues.interstitialAdUnitID)
 //        interstitialAd.delegate = self
 //        let request = GADRequest()
-//        
+//
 //        // TDOD remove test code
 //        request.testDevices = [ "b90d517bbd126ad43c612357a349ee23" ]
 //        interstitialAd.load(request)
 //        return interstitialAd
 //    }
-//    
+//
 //    /// Tells the delegate an ad request succeeded.
 //    func interstitialDidReceiveAd(_ ad: GADInterstitial) {
 //        print("interstitialDidReceiveAd")
 //    }
-//    
+//
 //    /// Tells the delegate an ad request failed.
 //    func interstitial(_ ad: GADInterstitial, didFailToReceiveAdWithError error: GADRequestError) {
 //        print("interstitial:didFailToReceiveAdWithError: \(error.localizedDescription)")
 //    }
-//    
+//
 //    /// Tells the delegate that an interstitial will be presented.
 //    func interstitialWillPresentScreen(_ ad: GADInterstitial) {
 //        print("interstitialWillPresentScreen")
 //    }
-//    
+//
 //    /// Tells the delegate the interstitial is to be animated off the screen.
 //    func interstitialWillDismissScreen(_ ad: GADInterstitial) {
 //        print("interstitialWillDismissScreen")
 //    }
-//    
+//
 //    /// Tells the delegate the interstitial had been animated off the screen.
 //    func interstitialDidDismissScreen(_ ad: GADInterstitial) {
 //        print("interstitialDidDismissScreen")
 //        interstitialAd = createAndLoadInterstitial()
 //        scoreScene?.presentScene()
 //    }
-//    
+//
 //    /// Tells the delegate that a user click will open another app
 //    /// (such as the App Store), backgrounding the current app.
 //    func interstitialWillLeaveApplication(_ ad: GADInterstitial) {
 //        print("interstitialWillLeaveApplication")
 //    }
-//    
+//
 //}
