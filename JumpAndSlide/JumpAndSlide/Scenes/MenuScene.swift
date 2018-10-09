976aee19d541f77d759c67c58fed60bfed514b04
Li Ju <sheldon.juli@gmail.com>
2018-10-09 11:31:03 -0400

    Added scenes

---
+++ JumpAndSlide/JumpAndSlide/Scenes/MenuScene.swift
@@ -0,0 +1,9 @@
+//
+//  MenuScene.swift
+//  JumpAndSlide
+//
+//  Created by Li Ju on 2018-10-09.
+//  Copyright © 2018 Li Ju. All rights reserved.
+//
+
+import Foundation
---
+++ JumpAndSlide/JumpAndSlide/Utility/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  JumpAndSlide
+//
+//  Created by Li Ju on 2018-10-09.
+//  Copyright © 2018 Li Ju. All rights reserved.
+//
+
+import Foundation
